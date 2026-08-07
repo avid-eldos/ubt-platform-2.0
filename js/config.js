572d4b9511583b0c566cc614685f039052f63a20
avid-eldos <avideldos2004@gmail.com>
2026-08-07 14:54:27 +0500

    2nd page

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 // Настройки проекта (Ключи и контакты)
 const CONFIG = {
     // данные из Supabase
-    SUPABASE_URL: 'https://pfpiuglesczumpdawgyf.supabase.co', 
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmcGl1Z2xlc2N6dW1wZGF3Z3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzOTA2MDUsImV4cCI6MjEwMDk2NjYwNX0.lkmUpUIk3YAmFmgAv40uCgvNEOY5nmCyHmsHTTCG6EA',
+    SUPABASE_URL: '', 
+    SUPABASE_KEY: '',
     
     // контакты для покупки курсов
     CONTACT_WHATSAPP: '77052160607',
