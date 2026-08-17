1de2435e6a62e6813888c8f4197406716bd33d27
avid-eldos <avideldos2004@gmail.com>
2026-08-17 14:27:24 +0500

    idk wtf was happened

---
+++ js/config.local.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://pfpiuglesczumpdawgyf.supabase.co', 
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmcGl1Z2xlc2N6dW1wZGF3Z3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzOTA2MDUsImV4cCI6MjEwMDk2NjYwNX0.lkmUpUIk3YAmFmgAv40uCgvNEOY5nmCyHmsHTTCG6EA',
+    CONTACT_WHATSAPP: '77052160607',
+    CONTACT_TELEGRAM: 'tyntynryn'
+};
